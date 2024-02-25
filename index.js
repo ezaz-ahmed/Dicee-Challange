2d0c34d4b6268023719878199eaa1aed5c22459b
Ezaz Ahmed <ezaznahmed@gmail.com>
2024-02-26 00:42:52 +0600

    feat: introduce Roll Dice button

--- index.js
+++ index.js
@@ -1,19 +1,28 @@
-var randomNumber1 = Math.floor(Math.random() * 6) + 1;
+const refreshButton = document.getElementById('refreshButton');
 
-var randomImageSource1 = "images/dice" + randomNumber1 + ".png";
+refreshButton.addEventListener('click', function () {
+    rollDice()
+});
 
-document.querySelectorAll("img")[0].setAttribute("src", randomImageSource1);
+function rollDice() {
+    var randomNumber1 = Math.floor(Math.random() * 6) + 1;
 
-var randomNumber2 = Math.floor(Math.random() * 6) + 1;
+    var randomImageSource1 = "images/dice" + randomNumber1 + ".png";
 
-var randomImageSource2 = "images/dice" + randomNumber2 + ".png";
+    document.querySelectorAll("img")[0].setAttribute("src", randomImageSource1);
 
-document.querySelectorAll("img")[1].setAttribute("src", randomImageSource2);
+    var randomNumber2 = Math.floor(Math.random() * 6) + 1;
 
-if (randomNumber1 > randomNumber2) {
-    document.querySelector("h1").innerHTML = "Player1 win";
-} else if (randomNumber1 < randomNumber2) {
-    document.querySelector("h1").innerHTML = "Player2 win";
-} else {
-    document.querySelector("h1").innerHTML = "Draw";
+    var randomImageSource2 = "images/dice" + randomNumber2 + ".png";
+
+    document.querySelectorAll("img")[1].setAttribute("src", randomImageSource2);
+
+    if (randomNumber1 > randomNumber2) {
+        document.querySelector("h1").innerHTML = "Player1 win";
+    } else if (randomNumber1 < randomNumber2) {
+        document.querySelector("h1").innerHTML = "Player2 win";
+    } else {
+        document.querySelector("h1").innerHTML = "Draw";
+    }
 }
+
